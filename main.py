736ece40d74faf88a26ccb006b78fa413e38709f
KajetanMieloch <kajetanmieloch@gmail.com>
2024-11-05 17:48:37 +0100

    done

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import pygame
 import sys
 import random
+import math
 
 class Disc:
-    def __init__(self, x, y, radius, color, dx, dy):
+    def __init__(self, x, y, radius, color, dx, dy, deltat):
         self.x = x
         self.y = y
         self.radius = radius
         self.color = color
         self.dx = dx
         self.dy = dy
+        self.deltat = deltat
 
     def draw(self, window):
         pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
 
-    def move(self, width, height, deltat):
-        self.x += self.dx * deltat
-        self.y += self.dy * deltat
+    def move(self, width, height, cx, cy):
+
+        Rc = 50
+
+        dist_x = cx - self.x
+        dist_y = cy - self.y
+        r = math.sqrt(dist_x**2 + dist_y**2)
+
+        if r < Rc:
+            r = Rc
+        
+        force = 20000 / (r**3 + 100)
+
+        fx = force * (dist_x)
+        fy = force * (dist_y)
+
+        self.dx += fx * self.deltat
+        self.dy += fy * self.deltat
+
+        self.x += self.dx * self.deltat
+        self.y += self.dy * self.deltat
 
         if self.x - self.radius <= 0 or self.x + self.radius >= width:
             self.dx = -self.dx
@@ -28,6 +48,9 @@ pygame.init()
 width, height = 800, 600
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Disc sim")
+deltat = 0.05
+
+cx, cy = width // 2, height // 2
 
 N = 1000
 discs = []
@@ -39,10 +62,10 @@ for _ in range(N):
     color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     dx = random.choice([-5, 5])
     dy = random.choice([-5, 5])
-    discs.append(Disc(x, y, radius, color, dx, dy))
+    deltat = deltat
+    discs.append(Disc(x, y, radius, color, dx, dy, deltat))
 
 running = True
-deltat = 0.1
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,7 +74,7 @@ while running:
     window.fill((0, 0, 0))
 
     for disc in discs:
-        disc.move(width, height, deltat)
+        disc.move(width, height, cx, cy)
         disc.draw(window)
 
     pygame.display.flip()
